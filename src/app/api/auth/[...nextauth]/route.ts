67a8b9ac5aa0042fee8a2713b0a6c2f8a51807bb
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-12-11 12:18:21 +0000

    stable authentication and register

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,11 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-import { Account,User as AuthUser } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import User from "@/models/User"
 import connect from "@/utils/db"
 import bcrypt from "bcryptjs"
-// interface NextAuthUser {
-//   name?: string | null;
-//   email?: string | null;
-//   image?: string | null;
-//   // Add other properties based on your needs
-// }
-
-// interface NextAuthAccount {
-//   provider: string;
-//   type: string;
-//   id: string;
-//   refreshToken?: string;
-//   accessToken: string;
-//   // Add other properties based on your needs
-// }
-// interface NextAuthProfile {
-//   id: string;
-//   name?: string | null;
-//   email?: string | null;
-//   image?: string | null;
-//   // Add other properties based on your needs
-// }
-
-// (params: {
-//   user: typeof User | null;
-//   account: Account | null;
-//   profile: any;
-//   email?: { verificationRequest?: boolean };
-//   credentials?: any;
-// }) => Promise<boolean>;
 
 const authOptions:NextAuthOptions = {
-  // Configure one or more authentication providers
   providers: [
     CredentialsProvider({
         id:"credentials",
@@ -63,46 +31,15 @@ const authOptions:NextAuthOptions = {
             }
         }
     }),
+
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
     
-    // ...add more providers here
+
   ],
-//   callbacks: {
-//     async signIn(params) {
-//     const { user, account, profile, email, credentials } = params;
-
-//       // Check if the user already exists in MongoDB based on their email
-//       const existingUser: typeof User | null = await User.findOne({
-//         email: profile?.email as string,
-//       });
-
-//       if (existingUser) {
-//         return existingUser;
-//       } else {
-//         const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$/;
-//         const randomPassword = () => Array.from({ length: 12 }, () => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&+="[Math.floor(Math.random() * 69)]).join('');
-//         const validRandomPassword = randomPassword();
-
-//         const hashedPassword =  bcrypt.hashSync(validRandomPassword,10);
-
-//         const newUser = await User.create({
-//           email: profile?.email,
-//           name: profile?.name || "name",
-//           image: profile?.image,
-//           password:hashedPassword,
-//           twitterUsername:"",
-//           githubUsername:"",
-//           linkedinUsername:"",
-//         });
-        
-        
-//         return newUser;
-//       }
-//     },
-// },
 }
+
 const handler =  NextAuth(authOptions);
 export {handler as GET, handler as POST}
