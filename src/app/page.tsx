5a8f4edd13e9d52e4dc6dea875880223f20c5824
Modou Aicha Diop <modouaichadiop@gmail.com>
2023-12-15 13:07:30 +0000

    Update home page UI, SVG and background image paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,23 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { MoveRightIcon } from "lucide-react";
 import Image from "next/image";
-import devStuffImage from "../../public/images/Hire-JS-Dev.svg";
+import devStuffImage from "../../public/images/Development-Team-coding.svg";
+import { useTheme } from "next-themes";
 
-export default  function Home() {  
+export default function Home() {
+  const { theme, setTheme } = useTheme();
+
   return (
     <Container>
-      <div className="flex w-full flex-col bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 border  gap-6 p-6">
-        <div className="w-full h-fit md:h-screen flex flex-col items-center justify-evenly gap-10 md:gap-6 p-8  bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-60 rounded-lg border-gray-100 border bg-green-700 ">
+      <div className="flex w-full flex-col bg-clip-padding  backdrop-filter backdrop-blur-lg bg-opacity-10   gap-6 p-6">
+        <div
+          className={` w-full min-h-screen flex flex-col items-center justify-evenly gap-10 md:gap-6 p-8  bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-90 rounded-lg ${
+            theme === "light"
+              ? "shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]"
+              : "border-gray-100"
+          }  border bg-black-700 `}>
           <div className="flex flex-col items-center justify-between md:flex-row gap-10">
-            <div className="flex flex-col gap-8 w-full md:w-1/2">
+            <div className="flex flex-col gap-8 w-full text-center md:text-left  md:w-1/2">
               <h1 className="font-bold text-3xl md:text-3xl">
                 Elevate your{" "}
                 <span className="text-yellow-500"> Coding Skills</span> with Our{" "}
@@ -43,7 +51,12 @@ export default  function Home() {
           </Button>
         </div>
 
-        <div className="w-full h-fit md:h-screen flex flex-col items-center justify-evenly gap-10 md:gap-6 p-8  bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-lg border-gray-100 border bg-gray-600 "></div>
+        <div
+          className={` w-full min-h-screen flex flex-col items-center justify-evenly gap-10 md:gap-6 p-8  bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-90 rounded-lg ${
+            theme === "light"
+              ? "shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]"
+              : "border-gray-100"
+          }  border bg-black-700 `}></div>
       </div>
     </Container>
   );
